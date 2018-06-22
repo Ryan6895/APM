3badc6b4e2b80876f9d5cfbd5f9ecec96dacf092
Your NameRyan <Rymaster95@gmail.com>
2018-06-21 20:33:50 -0400

    Finished simple Angular Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from "./home/welcome.component";
+import { ProductModule } from './products/product.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'Welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'Welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'Welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
